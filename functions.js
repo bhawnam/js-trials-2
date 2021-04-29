65bea7119ca363137e92595215198e466fca7f92
Bhawna Mulchandani <bhawna.bm@gmail.com>
2021-04-29 03:56:47 +0000

    Fixed the JS syntax

--- functions.js
+++ functions.js
@@ -7,18 +7,19 @@ function isHometown(town){
     return town === 'San Francisco';
 }
 
-const homeTown = isHometown('Dubai');
-console.log(homeTown)
+const homeTown = isHometown('Delhi');
+console.log(homeTown);
 
 // 2. getFullName
 
 function getFullName(firstname, lastname){
-    return (`${firstname} ${lastname} `)
+    return `${firstname} ${lastname}`;
 }
 
-const fullName  = getFullName('Gunjan' , 'Amarnani')
-console.log(fullName)
-
+const fullName1  = getFullName('Gunjan' , 'Amarnani');
+const fullName2  = getFullName('Bhawna' , 'Mulchandani');
+console.log(fullName1);
+console.log(fullName2);
 
 // 3. calculateTotal
 
@@ -30,15 +31,15 @@ function calculateTotal(basePrice, state, tax=0.05){
         fee = 0.03 * subTotal;
     } else if (state === 'PA'){
         fee = 2;
-    }else if (state === 'MA'){
+    } else if (state === 'MA'){
         if (basePrice <= 100){
             fee = 1;
-        }else{
+        } else {
             fee = 3;
         }
     }    
     return subTotal + fee
 }
 
-const total = calculateTotal(100, 'MA')
-console.log(total)
+const total = calculateTotal(100, 'MA');
+console.log(total);
